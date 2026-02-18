6f675f7e8988fc25e4cae845b2449df980bb734c
Jacob201435 <158504205+Jacob201435@users.noreply.github.com>
2026-02-18 11:06:03 -0600

    Add files via upload

---
+++ Dependencies/wait.h
@@ -0,0 +1,20 @@
+/*
+ * wait.h
+ *
+ *  Created on: Feb 1, 2025
+ *      Author: bdavis
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "clock.h"
+
+
+#ifndef WAIT_H_
+#define WAIT_H_
+
+
+void waitMicrosecond(uint32_t usec);
+
+
+#endif /* WAIT_H_ */
